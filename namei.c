3f52c6dabdb5b4ed0b98bd3cf4f443ef68c8f125
nguyenhuy-vn <167416480+nguyenhuy-vn@users.noreply.github.com>
2025-02-09 17:40:15 +0700

    Update namei.c

--- namei.c
+++ namei.c
@@ -43,25 +43,7 @@
 #include <linux/ktime.h>
 #include <linux/string.h>
 
-//ham doi ten
-static unsigned long ext2_check_free_space1(struct super_block *sb){
-    struct ext2_sb_info *sbi = EXT2_SB(sb);
-    struct ext2_super_block *es = sbi->s_es;
-    unsigned long free_blocks;
-    unsigned long reserved_blocks;
-    unsigned long available_blocks;
-
-    free_blocks = le32_to_cpu(es->s_free_blocks_count);
-    reserved_blocks = le32_to_cpu(es->s_r_blocks_count);
-
-    if (free_blocks > reserved_blocks) {
-        available_blocks = free_blocks - reserved_blocks;
-    } else {
-        available_blocks = 0;
-    }
-    // Trả về dung lượng trống tính bằng bytes
-    return available_blocks * sb->s_blocksize;  // Đổi từ blocks sang bytes
-}
+// rename function
 static inline char to_upper(char c)
 {
 	if (c >= 'a' &&	c <= 'z')
